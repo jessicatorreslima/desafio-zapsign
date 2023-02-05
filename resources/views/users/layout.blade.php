ec4b8ce4d8e6248ca42f9e47c79c2364c21eead6
jessicatorreslima <jtlimapro@gmail.com>
2023-02-05 15:56:46 -0300

    Add bootstrap 5

--- resources/views/users/layout.blade.php
+++ resources/views/users/layout.blade.php
@@ -2,6 +2,12 @@
 <html>
 <head>
     <title> Desafio ZapSign - By Jéssica Torres </title>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
   
