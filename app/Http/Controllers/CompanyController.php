aa32f6d58428e8ff71a72ee9fe265552ea4b34e5
jessicatorreslima <jtlimapro@gmail.com>
2023-02-12 20:28:16 -0300

    Change company pagination to 10

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -15,7 +15,7 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        $companies = Company::oldest()->paginate(5);
+        $companies = Company::oldest()->paginate(10);
     
         return view('companies/index',compact('companies'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
