a0281eefd9bed95305242cd0813609565a09176a
jessicatorreslima <jtlimapro@gmail.com>
2023-02-12 20:00:43 -0300

    Add 'doc' factory and seeder

---
+++ database/factories/DocFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Doc;
+use App\Models\User;
+use App\Models\Company;
+
+class DocFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Doc::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //'name' => $this->php('/temp', '/temp', false),
+            'name' => $this->faker->lexify('???????????').'.pdf',
+            'date_limit_to_sign' => $this->faker->dateTimeThisYear(),
+            'company_id' => Company::factory(),
+            'created_by' => User::factory()
+        ];
+    }
+}
